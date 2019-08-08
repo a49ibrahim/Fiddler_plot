0e7b926645e9578cb282b1ca12d87ad8f1e7b71f
a49ibrahim <53889037+a49ibrahim@users.noreply.github.com>
2019-08-07 21:53:04 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.scss';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './Nav';
+import Generate from './Generate';
+import Dashboard from './Dashboard';
+
+class App extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			savedPlots: [],
+		};
+		this.updateSavedPlots = this.updateSavedPlots.bind(this);
+	}
+	updateSavedPlots(savedPlots){
+		this.setState( savedPlots);
+	}
+	render(){
+		return(
+			<Router>
+				<div className='App'>
+				<Nav />
+					<Switch>
+						<Route exact path="/" render={props =>
+						<Generate savedPlots={this.state.savedPlots}
+						setSavedPlots ={this.updateSavedPlots} /> }/>
+
+						<Route path='/dashboard' render={props =>
+						<Dashboard savedPlots={this.state.savedPlots}/>} />
+						<Route render={function () {
+							return <p>404 Page Not Found </p>
+						}} />
+						//}
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
+
+}
+
+export default App;
